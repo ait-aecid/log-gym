26695a770713c530b1b9e3bbd00f41b046ccae8b
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2024-12-17 14:34:32 +0100

    ADD data_analysis notebook

---
+++ notebooks/plot_methods.py
@@ -0,0 +1,152 @@
+
+import matplotlib.pyplot as plt 
+import networkx as nx
+import pandas as pd 
+import numpy as np
+import math
+
+# %% Event statistics
+
+def get_length_dist(df):
+    event_seq = df["Event ID"]
+    event_length = event_seq.apply(lambda x: len(eval(x)))
+    values = list(pd.unique(event_length))
+    heights = np.zeros(len(values))
+
+    for e in event_length:
+        heights[values.index(e)] += 1
+    values = np.array(values)
+    idx = np.argsort(values)
+
+    return values[idx], heights[idx]
+
+    
+def unique_elements(df):
+    bag_words = {}
+    event_seq = df["Event ID"]
+    for seq in event_seq:
+        for elem in eval(seq):
+            bag_words[elem] = bag_words.get(elem, 0) + 1
+
+    return bag_words
+
+
+def get_unique_elements(df):
+    unique = unique_elements(df)
+
+    idx = np.flip(np.argsort(list(unique.values())))
+    values = np.array(list(unique.keys()))[idx]
+    heights = np.array(list(unique.values()))[idx]
+
+    return values, heights
+
+
+def plot_events(tables):
+    fig, axs = plt.subplots(1, 2, figsize=(14, 5))
+    fig.suptitle("Event statistics", fontsize=20)
+
+    for label, path in tables.items():
+        df = pd.read_csv(path)
+
+        values_length, heights_length = get_length_dist(df)
+        axs[0].bar(values_length, heights_length, alpha=0.4)
+        axs[0].plot(values_length, heights_length, label=label)
+
+        values_unique, heights_unique = get_unique_elements(df)
+        axs[1].bar(values_unique.astype(str), heights_unique, alpha=0.4)
+
+    axs[0].grid(), axs[1].grid()
+    axs[0].legend()
+
+    axs[0].set_xlabel("Length event sequence", fontsize=14)
+    axs[0].set_ylabel("Amount of event sequences", fontsize=14)
+    axs[1].set_xlabel("Unique events", fontsize=14)
+    axs[1].set_ylabel("Amount of events", fontsize=14)
+
+
+# %% Time difference
+
+def plot_violin(df, ax, do_xlabel=True, do_ylabel=True):
+    time_diff = df["Time Diff"].apply(lambda x: eval(x))
+    event_seqs = df["Event ID"].apply(lambda x: eval(x)[1:])
+
+    stats = {}
+    for events, times in zip(event_seqs, time_diff):
+        for event, time in zip(events, times):
+            if event not in stats.keys():
+                stats[event] = []
+            stats[event].append(time)
+
+
+    values = list(stats.keys())
+    times = list(stats.values())
+
+    ax.violinplot(times)
+    ax.set_xticks(range(1, len(values) + 1), np.array(values).astype(str))
+    ax.grid()
+    if do_xlabel:
+        ax.set_xlabel("Event ID", fontsize=17)
+    if do_ylabel:
+        ax.set_ylabel("Time range", fontsize=17)
+
+
+def plot_time_diff(tables):
+    rows = math.ceil(len(tables) / 2)
+    _, axs = plt.subplots(rows, 2, figsize=(14, 10))
+
+    for i, (name, path) in enumerate(tables.items()):
+        row = math.floor(i / rows)
+        column = i % rows
+        axs[row, column].set_title(name, fontsize=18)
+
+        plot_violin(
+            df=pd.read_csv(path), 
+            ax=axs[row, column],
+            do_xlabel=row == (rows - 1),
+            do_ylabel=column == 0
+        )
+
+
+# %% Level log distribution
+
+def plot_pie(df, ax):
+    levels_seq = df["Level"].apply(lambda x: eval(x))
+
+    stats = {}
+    for levels in levels_seq:
+        for level in levels:
+            stats[level] = stats.get(level, 0) + 1
+
+
+    levels = list(stats.keys())
+    values = list(stats.values())
+
+    ax.pie(values, labels=levels, autopct="%1.1f%%")
+
+
+def plot_level_dist(tables):
+    rows = math.ceil(len(tables) / 2)
+    _, axs = plt.subplots(rows, 2, figsize=(14, 10))
+    
+    for i, (name, path) in enumerate(tables.items()):
+        row = math.floor(i / rows)
+        column = i % rows
+        axs[row, column].set_title(name, fontsize=18)
+        plot_pie(df=pd.read_csv(path), ax=axs[row, column])
+
+
+# %% Execturion plot graph
+ 
+def plot_execution_graph(tables):
+    for i, (name, path) in enumerate(tables.items()):
+        df = pd.read_csv(path)
+        content_seqs = df["Content"].apply(lambda x: eval(x))
+
+        G = nx.DiGraph()
+        for content_seq in content_seqs:
+            for c1, c2 in zip(content_seq[:-1], content_seq[1:]):
+                G.add_edge(c1, c2)
+
+        plt.figure(i, figsize=(20, 5))
+        plt.title(name, fontsize=18)
+        nx.draw(G, with_labels = True)
