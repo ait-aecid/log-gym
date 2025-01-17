77234a31af5202e503eff6d7cd5e4c0d4c8ab972
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-01-17 14:06:36 +0100

    ADD case 1 of collaborative simulation

--- simulations/collaborative_setup/methods.py
+++ simulations/collaborative_setup/methods.py
@@ -2,11 +2,45 @@
 from backbone.msg_reader import Messages
 import backbone.logs as logs
 
+from typing import Generator
+import time
 
-def case_1_wrong_template(
-    do_anomaly: bool, msg: Messages, client_n: int, is_test: bool = False, 
-) -> None:
+
+class Sender:
+    def __init__(
+        self, do_anomaly: bool, msg: Messages, client_n: int, is_test: bool = False
+    ) -> None:
+        self.do_anomaly = do_anomaly
+        self.msg = msg
+        self.template = 1 if client_n == 0 else 2
+        self.is_test = is_test
+
+    def wait_to_send(self) -> Generator[bool, None, None]:
+        wait_amount = 10 if self.do_anomaly else 4
+        for _ in range(wait_amount):
+            logs.info(self.msg.waiting)
+            if not self.is_test:
+                time.sleep(0.05)
+            yield True
+        yield False
+
+    def send_package(self) -> None:
+        if self.do_anomaly:
+            logs.info(self.msg[f"template_{self.template}_error"])
+        else:
+            logs.info(self.msg[f"template_{self.template}_send_file"])
+        
+
+
+def case_1_wrong_template(do_anomaly: bool, msg: Messages, client_n: int) -> None:
     """
     One of the clients creates a wrong template in the parsing
     """
-    logs.info(msg.test_simulation)
+    sender = Sender(do_anomaly=do_anomaly, msg=msg, client_n=client_n)
+
+    logs.info(msg.prepared_to_send)
+    for _ in range(4):
+        wait_process = sender.wait_to_send()
+        while next(wait_process): pass
+        sender.send_package()
+    
---
+++ test/test_simulation_collaborative.py
@@ -0,0 +1,93 @@
+from simulations.collaborative_setup import methods
+
+import unittest
+
+
+path_msg = "simulations/collaborative_setup/messages.yaml"
+
+class SenderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.msg = methods.Messages.from_file(path_msg, version=1)
+
+    def test_sender_template_1(self):
+        import backbone.logs as logs
+        logs.store_logs = False
+        logs.as_test()
+        logs.history = []
+
+        sender = methods.Sender(do_anomaly=False, msg=self.msg, client_n=0)
+        sender.send_package()
+
+        self.assertListEqual(
+            [" INFO:Test <*template_1*>Sending file with package 1"], logs.history
+        )
+
+    def test_sender_template_2(self):
+        import backbone.logs as logs
+        logs.store_logs = False
+        logs.as_test()
+        logs.history = []
+
+        sender = methods.Sender(do_anomaly=False, msg=self.msg, client_n=1)
+        sender.send_package()
+        sender = methods.Sender(do_anomaly=False, msg=self.msg, client_n=2)
+        sender.send_package()
+
+        self.assertListEqual(
+            [
+                " INFO:Test <*template_2*>Sending file with package 1",
+                " INFO:Test <*template_2*>Sending file with package 1",
+            ], logs.history
+        )
+
+    def test_sender_template_1_error(self):
+        import backbone.logs as logs
+        logs.store_logs = False
+        logs.as_test()
+        logs.history = []
+
+        sender = methods.Sender(do_anomaly=True, msg=self.msg, client_n=0)
+        sender.send_package()
+
+        self.assertListEqual(
+            [" INFO:Test <*template_1*>Sending file with package 1 Failed"], logs.history
+        )
+
+    def test_sender_template_2_error(self):
+        import backbone.logs as logs
+        logs.store_logs = False
+        logs.as_test()
+        logs.history = []
+
+        sender = methods.Sender(do_anomaly=True, msg=self.msg, client_n=1)
+        sender.send_package()
+        sender = methods.Sender(do_anomaly=True, msg=self.msg, client_n=2)
+        sender.send_package()
+
+        self.assertListEqual(
+            [
+                " INFO:Test <*template_2*>Sending file with package 1 Failed",
+                " INFO:Test <*template_2*>Sending file with package 1 Failed",
+            ], logs.history
+        )
+
+    def test_sender_wait(self):
+        import backbone.logs as logs
+        logs.store_logs = False
+        logs.as_test()
+        logs.history = []
+
+        sender = methods.Sender(do_anomaly=False, msg=self.msg, client_n=0, is_test=True)
+        gen = sender.wait_to_send()
+        while next(gen): pass
+        self.assertListEqual(
+            [" INFO:Test Waiting..." for _ in range(4)], logs.history
+        )
+
+        logs.history = []
+        sender = methods.Sender(do_anomaly=True, msg=self.msg, client_n=0, is_test=True)
+        gen = sender.wait_to_send()
+        while next(gen): pass
+        self.assertListEqual(
+            [" INFO:Test Waiting..." for _ in range(10)], logs.history
+        )
